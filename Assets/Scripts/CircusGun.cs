b2e175ad250039d42e7520708e2e9589a139e417
TinDrumQuincy <qcampbell@tindrum.io>
2024-01-27 22:42:27 -0500

    fire rate and lower enemy health

--- Assets/Scripts/CircusGun.cs
+++ Assets/Scripts/CircusGun.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class CircusGun : MonoBehaviour
 {
     public Transform muzzleTransform;
+    public float fireRate = 0.7f;
 
     protected virtual void DoFire() { }
 
+    private float lastFireTime;
     public void StartFire()
     {
-        DoFire();
+        if (Time.time > lastFireTime + fireRate)
+        {
+            DoFire();
+            lastFireTime = Time.time;
+        }
     }
     
     public void StopFire()
